cb1bac457f099605d14e3f1407becf203897efdf
Jennifer Nerlich <Jennifer.Nerlich@vogella.com>
2023-12-19 11:08:53 +0100

    Conversion of Evolving Java  based APIs

--- migrate.dart
+++ migrate.dart
@@ -5,7 +5,7 @@ import 'package:html2md/html2md.dart' as html2md;
 import 'dart:io';
 
 Future<void> main() async {
-  var wikiPageUrl = 'https://wiki.eclipse.org/JFaceSnippets';
+  var wikiPageUrl = 'https://wiki.eclipse.org/Evolving_Java-based_APIs';
   final imagePageUrl = 'https://wiki.eclipse.org';
 
   try {
